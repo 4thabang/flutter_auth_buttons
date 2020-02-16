e616b2e388087f2e7646803c84d52fc1d7500ce6
4thabang <58977471+4thabang@users.noreply.github.com>
2020-02-16 11:36:06 +0000

    Add files via upload

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/thabang/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/thabang/flutter_auth_buttons/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/thabang/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ example/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+void main() async {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    const padding = 25.0;
+
+    return MaterialApp(
+      title: 'Button Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("flutter_auth_buttons"),
+        ),
+        backgroundColor: Color.fromARGB(0xFF, 0xF0, 0xF0, 0xF0),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  SizedBox(height: padding),
+                  AppleSignInButton(onPressed: () {}),
+                  AppleSignInButton(
+                      onPressed: () {}, style: AppleButtonStyle.whiteOutline),
+                  AppleSignInButton(
+                      onPressed: () {}, style: AppleButtonStyle.black),
+                  SizedBox(height: padding),
+                  GoogleSignInButton(onPressed: () {}),
+                  GoogleSignInButton(onPressed: () {}, darkMode: true),
+                  SizedBox(height: padding),
+                  FacebookSignInButton(onPressed: () {}),
+                  SizedBox(height: padding),
+                  TwitterSignInButton(onPressed: () {}),
+                  SizedBox(height: padding),
+                  MicrosoftSignInButton(onPressed: () {}),
+                  MicrosoftSignInButton(onPressed: () {}, darkMode: true),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  Heading(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
